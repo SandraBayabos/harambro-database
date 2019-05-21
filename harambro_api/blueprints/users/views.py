a53f1292f96cb51711ae1a840493150735353c6a
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-05-21 10:41:48 +0800

    added harambro_api folder

---
+++ harambro_api/blueprints/users/views.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, jsonify
+from flask_jwt import JWT, jwt_required
+from models.user import User
+
+users_api_blueprint = Blueprint('users_api',
+                                __name__,
+                                template_folder='templates')
+
+
+@users_api_blueprint.route('/', methods=['GET'])
+def index():
+    return "USERS API"
+
+
+@users_api_blueprint.route('/<email>', methods=['GET'])
+@jwt_required
+def show(email):
+    user = User.get_or_none(User.email == email)
+
+    if not user:
+        resp = {
+            'message': 'No user found with this email',
+            'ok': False
+        }
+        # use jsonify to return a response in a JSON format
+        return jsonify(resp)
+
+    resp = {
+        'message': 'Found user with this email',
+        'user': {
+            'id': user.id,
+            'name': user.name,
+            'email': user.email,
+
+        },
+        'okay': True
+    }
+
+    return jsonify(resp)
