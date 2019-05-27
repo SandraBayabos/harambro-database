63ef1b0dc841daac406490832efbb7fc01c542f3
Victoria Lee <vickyloon91@gmail.com>
2019-05-27 21:58:07 +0800

    implemented botostrap

--- harambro_web/blueprints/sessions/views.py
+++ harambro_web/blueprints/sessions/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user, U
 from models.user import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from harambro_web.util.google_oauth import oauth
-
+from flask_bootstrap import Bootstrap
 sessions_blueprint = Blueprint('sessions',
                                __name__,
                                template_folder='templates')
