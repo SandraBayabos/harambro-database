f01f0cc3e6235bc0c85b97d82681bfad108cea5b
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-05-31 12:01:39 +0800

    changed date

--- models/base_model.py
+++ models/base_model.py
@@ -2,18 +2,21 @@ import os
 import peewee as pw
 import datetime
 from database import db
+from datetime import timedelta
 
 
 class BaseModel(pw.Model):
-    created_at = pw.DateTimeField(default=datetime.datetime.now)
-    updated_at = pw.DateTimeField(default=datetime.datetime.now)
+    created_at = pw.DateTimeField(
+        default=datetime.datetime.now + timedelta(hours=8))
+    updated_at = pw.DateTimeField(
+        default=datetime.datetime.now + timedelta(hours=8))
 
     def save(self, *args, **kwargs):
         self.errors = []
         self.validate()
 
         if len(self.errors) == 0:
-            self.updated_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now() + timedelta(hours=8)
             return super(BaseModel, self).save(*args, **kwargs)
         else:
             return 0
